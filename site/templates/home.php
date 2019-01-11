537a62c8d91f124843b90c084b9eacd07592f22c
Walter Robin <gauthierroussilhe@protonmail.com>
2019-01-11 15:26:02 +0000

    Update logos

--- site/templates/home.php
+++ site/templates/home.php
@@ -101,13 +101,22 @@
                 <img src="assets/images/55white.png">
             </div>
             <div class="three columns">
-                <img src="assets/images/paypalwhite.png">
+                <img src="assets/images/criteowhite.png">
             </div>
             <div class="three columns">
                 <img src="assets/images/prontowhite.png">
             </div>
             <div class="three columns">
                 <img src="assets/images/ubisoftwhite.png">
+            </div>
+            <div class="three columns">
+                <img src="assets/images/adikteev.png">
+            </div>
+            <div class="three columns">
+                <img src="assets/images/arcane.png">
+            </div>
+            <div class="three columns">
+                <img src="assets/images/easymovie.png">
             </div>
         </div>
         <hr>
