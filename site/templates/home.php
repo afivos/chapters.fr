974ab74ed993e2da8ec3b0ae428a6c9f43d6c57b
Fivos Avgerinos <afivos@me.com>
2019-01-24 19:08:06 +0000

    Editable homepage!

--- site/templates/home.php
+++ site/templates/home.php
@@ -2,20 +2,28 @@
 
 <div class="container image-overlay image-overlay-home">
     <figure class="hidden-xs">
-        <img src="assets/images/home2.png">
+
+<?php
+// try to get an image object
+$image = $page->mainImg()->toFile();
+// check if image exists
+if($image): ?>
+<img src="<?php echo $image->url(); ?>" alt="">
+<?php endif ?>
+
     </figure>
 </div>
 
 <div class="container section-set text-center">
     <div class="row">
         <div class="four columns section section-grey">
-            <p>Un programme et un contenu au service de vos enjeux business</p>
+            <?= $page->feature1()->kirbytext() ?>
         </div>
         <div class="four columns section section-orange-light">
-            <p>Des solutions concrètes et actionnables, appuyées sur de solides bases théoriques</p>
+            <?= $page->feature2()->kirbytext() ?>
         </div>
         <div class="four columns section section-grey">
-            <p>Une approche actuelle du management (notre écosystème : Tech  &amp; Startup)</p>
+            <?= $page->feature3()->kirbytext() ?>
         </div>
     </div>
 </div>
@@ -26,7 +34,7 @@
             <span class="dot dot-left"></span>
         </div>
         <div class="ten columns text-center">
-            <h1>Un nouveau chapitre <br clear="hidden-xs">dans l'histoire de votre équipe</h1>
+            <?= $page->heading1()->kirbytext() ?>
         </div>
         <div class="one columns text-right">
             <span class="dot dot-right"></span>
@@ -38,42 +46,21 @@
     <div class="container">
         <div class="row">
             <div class="six columns section section-grey">
-                <h4>Renforcer votre middle-management pour vous développer</h4>
-                <ul>
-                    <li><p>Former vos équipes à la posture managériale pour en faire des relais de croissance performants</p></li>
-                    <li><p>Accompagner l'hypercroissance (scale-up)</p></li>
-                </ul>
+                <?= $page->list1()->kirbytext() ?>
             </div>
             <div class="six columns section section-orange">
-                <h4>Asseoir votre posture en tant que leader</h4>
-                <ul>
-                    <li><p>Développer vos performances à travers le coaching individuel</p></li>
-                    <li><p>Augmenter votre impact grâce à des outils de développement individuel</p></li>
-                </ul>
+                <?= $page->list2()->kirbytext() ?>
             </div>
         </div>
         <div class="row">
             <div class="six columns section section-grey visible-sm">
-                <h4>Fidéliser vos équipes</h4>
-                <ul>
-                    <li><p>Formaliser et partager une culture managériale à votre image</p></li>
-                    <li><p>Utiliser la formation comme levier de fidélisation</p></li>
-                </ul>
+                <?= $page->list3()->kirbytext() ?>
             </div>
             <div class="six columns section section-orange">
-                <h4>Accompagner le changement</h4>
-                <ul>
-                    <li><p>Préparer, planifier et déployer de façon efficace une réorganisation ou une reprise d'équipe</p></li>
-                    <li><p>Fiabiliser les messages et leurs communications</p></li>
-                    <li><p>Favoriser l'appropriation par l'accompagnement</p></li>
-                </ul>
+                <?= $page->list4()->kirbytext() ?>
             </div>
             <div class="six columns section section-grey hidden-sm">
-                <h4>Fidéliser vos équipes</h4>
-                <ul>
-                    <li><p>Formaliser et partager une culture managériale à votre image</p></li>
-                    <li><p>Utiliser la formation comme levier de fidélisation</p></li>
-                </ul>
+                <?= $page->list3()->kirbytext() ?>
             </div>
         </div>
     </div>
@@ -85,7 +72,7 @@
             <span class="dot dot-left"></span>
         </div>
         <div class="ten columns text-center">
-            <h1><a href="<?= $site->url() ?>/contact">Contactez-nous</a></h1>
+            <?= $page->heading2()->kirbytext() ?>
         </div>
         <div class="one columns text-right">
             <span class="dot dot-right"></span>
@@ -97,53 +84,39 @@
     <div class="container hidden-sm">
         <h1>Ils nous font confiance :</h1>
         <div class="row text-center">
+        <?php
+        $partners = $page->partners()->toStructure();
+        foreach($partners as $partner): ?>
             <div class="three columns">
-                <img src="assets/images/ubisoftwhite.png">
+                <img src="<?php echo $partner->image()->toFile()->url() ?>">
             </div>
-            <div class="three columns">
-                <img src="assets/images/criteowhite.png">
-            </div>
-            <div class="three columns">
-                <img src="assets/images/55white.png">
-            </div>
-            <div class="three columns">
-                <img src="assets/images/adikteev.png">
-            </div>
-            <div class="three columns">
-                <img src="assets/images/easymovie.png">
-            </div>
-            <div class="three columns">
-                <img src="assets/images/arcane.png">
-            </div>
-            <div class="three columns">
-                <img src="assets/images/prontowhite.png">
-            </div>
-            <div class="three columns">
-                <img src="assets/images/ppcom-white.svg">
-            </div>
+        <?php endforeach ?>
         </div>
     </div>
     <div class="container">
         <hr>
         <h1>Quelques mots de nos clients :</h1>
         <div class="row">
+            <!---------------------
+                <?php foreach($page->testimonials()->toStructure() as $testimonial): ?>
+                <div class="seven columns">
+                  <?= $testimonial->text()->kirbytext() ?>
+                </div>
+                <?php endforeach ?>
+            ----------------------->
             <div class="six columns">
-                <p>« Pierre a accompagné mon équipe pour faire évoluer le rôle attendu des Managers. Le programme a été une vraie réussite tant par l’adhésion qu’il a permis autour du projet que pour les techniques et méthodes que Pierre a su transmettre et qui sont aujourd’hui autant de références partagées qui nous permettent de travailler de manière plus fluide et plus efficace. »</p>   
-                <p>CH Hénault, Directeur de Analytics and Data Science chez Criteo</p>
+                <?= $page->testimonial1()->kirbytext() ?>
             </div>
             <div class="six columns">
-                <p>« A un moment clé du développement de notre entreprise, Chapters nous a accompagné dans la conception et la formalisation de notre vision de notre culture managériale. Grâce à cet accompagnement professionnel, attentif et respectueux de notre histoire nous avons réussi à construire avec nos équipes, le projet qui nous guide aujourd'hui. »</p>   
-                <p>Clément Marcorelles, Président de Wine Services</p>
+                <?= $page->testimonial2()->kirbytext() ?>
             </div>
         </div>
         <div class="row">
             <div class="six columns">
-                <p>« En tant que startup en hyper-croissance, il était prioritaire pour nous de structurer rapidement notre management et de développer nos managers intermédiaires. Cela nous permettait aussi pour nous de prendre du recul en tant qu’associés. En co-construisant notre culture managériale et en nous formant à des outils opérationnels de team-building et de management, Chapters nous a accompagné vers une nouvelle dimension dans notre développement. »</p>   
-                <p>Arthur D’Yvoire, CEO d’Arcane</p>
+                <?= $page->testimonial3()->kirbytext() ?>
             </div>
             <div class="six columns">
-                <p>« Des formations dynamiques et très concrètes pour nos managers et notre Comité de Direction, riches en solutions opérationnelles. »</p>   
-                <p>Anne-Sophie Madoire-Rouzaud, DRH Groupe Adikteev</p>
+                <?= $page->testimonial4()->kirbytext() ?>
             </div>
         </div>
     </div>
@@ -152,12 +125,7 @@
 <div class="container-image container-image-people">
     <div class="container">
         <div class="section section-orange">
-            <h4>Chapters, c'est aussi des solutions clé en main de séminaires team-building ou de conventions annuelles :</h4>
-            <ul>
-                <li><p>Un moment fort pour des souvenirs inoubliables</p></li>
-                <li><p>Des formules qui mêlent efficacement sessions de travails et moments festifs</p></li>
-                <li><p>Format 2-3 jours dans des grandes villes françaises ou européennes (City-Trip)</p></li>
-            </ul>
+            <?= $page->list5()->kirbytext() ?>
         </div>
     </div>
 </div>
