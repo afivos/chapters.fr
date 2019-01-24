a82357cfe6ffc76adc08420880cd363cb97e6f46
Fivos Avgerinos <afivos@me.com>
2019-01-24 16:13:43 +0000

    Update bio

--- site/templates/a-propos.php
+++ site/templates/a-propos.php
@@ -1,5 +1,46 @@
 <?php snippet('header-long') ?>
 
+<script>
+/*
+Ultimate Age calculator script- By JavaScript Kit (http://www.javascriptkit.com)
+Over 200+ free scripts here!
+Credit must stay intact for use
+*/
+
+var one_day=1000*60*60*24
+var one_month=1000*60*60*24*30
+var one_year=1000*60*60*24*30*12
+
+function displayage(yr, mon, day, unit, decimal, round){
+today=new Date()
+var pastdate=new Date(yr, mon-1, day)
+
+var countunit=unit
+var decimals=decimal
+var rounding=round
+
+finalunit=(countunit=="days")? one_day : (countunit=="months")? one_month : one_year
+decimals=(decimals<=0)? 1 : decimals*10
+
+if (unit!="years"){
+if (rounding=="rounddown")
+	document.write(Math.floor((today.getTime()-pastdate.getTime())/(finalunit)*decimals)/decimals+" "+countunit)
+else
+	document.write(Math.ceil((today.getTime()-pastdate.getTime())/(finalunit)*decimals)/decimals+" "+countunit)
+}
+else{
+	yearspast=today.getFullYear()-yr-1
+	tail=(today.getMonth()>mon-1 || today.getMonth()==mon-1 && today.getDate()>=day)? 1 : 0
+	pastdate.setFullYear(today.getFullYear())
+	pastdate2=new Date(today.getFullYear()-1, mon-1, day)
+	tail=(tail==1)? tail+Math.floor((today.getTime()-pastdate.getTime())/(finalunit)*decimals)/decimals : Math.floor((today.getTime()-pastdate2.getTime())/(finalunit)*decimals)/decimals
+	document.write(yearspast+tail+" "+countunit)
+	}
+}
+</script>
+
+
+
 <div class="container container-light-grey away-from-top section-contact">
 	<h3><?= $page->title()->html() ?></h3>
 
@@ -8,9 +49,18 @@
 			<img src="assets/images/photocontact.png">
 		</div>
 		<div class="six columns">
-			<p>Pierre Le Leuch, 38 ans, 6 ans d’expérience en coaching et accompagnement managérial, 7 ans de finance d’entreprise en contexte international (Mexico, New York), Passionné de kite surf, stand-up, bande dessinées et sciences sociales, Trilingue ENG-ESP-FR, HEC 2005.</p>
+			<h2>Pierre Le Leuch</h2>
+			<p><script>displayage(1981, 1, 1, "ans", 0, "rounddown")</script></p>
+			<p>Passionné par le management sous toutes ses formes, je pratique le coaching en entreprise et l’accompagnement managérial depuis 6 ans. J’ai eu la chance d’intervenir dans des secteurs variés (luxe, retail, media) et des contextes enrichissants (hypercroissance, PSE, enjeux syndicaux). Un peu geek sur les bords et avide de comprendre les changements qui nous entourent, je me suis spécialisé dans les secteurs des nouvelles technologies et des start-up.</p>
+			<p>Avant cela, j’ai connu une première vie professionnelle de 7 ans dans le monde de la finance d’entreprise, en cabinet et en direction financière, dans des contextes internationaux (Mexique, New York City).</p>
+			<p>En 2008, j’ai assisté à l’histoire en voyant des fenêtres de mon bureau les employés de Lehman Brothers sortir avec leur carton. Je me suis dit que c’était la fin d’une époque ! ;)</p>
+			<p>Aujourd’hui, quand je ne travaille pas, je profite de mes autres passions pour me ressourcer : le kite-surf, le stand-up (les spectacles, pas le paddle), les bandes dessinées, les sciences sociales (livres, podcasts, conférences..) et les jeux vidéos.</p>
+			<p>Trilingue Anglais-Espagnol-Français</p>
+			<p>Diplômé d’HEC 2005</p>
 		</div>
 	</div>
+
+	<hr>
 
 	<div class="row">
 		<div class="six columns">
